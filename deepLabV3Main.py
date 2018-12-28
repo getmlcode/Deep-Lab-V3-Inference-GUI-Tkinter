e6b8f3d42975211fa8695318cae5f22f79d69a11
siddharthsvnit <siddharthsvnit@gmail.com>
2018-12-28 23:49:00 +0530

    completed GUI development

--- deepLabV3Main.py
+++ deepLabV3Main.py
@@ -9,30 +9,56 @@ class deepLabV3_GUI:
     SegmentedImage=[]
     def __init__(self,master):
         master.title('Semantic Segmentation : Deep Lab V3')
-        master.minsize(500, 200)
+        master.minsize(200, 200)
+
+        #Setting Up Test Image Panel
 
         self.testImgFrame = Frame(master,highlightbackground="green",
                              highlightcolor="green",
-                             highlightthickness=2)
-        self.testImgFrame.pack()
-        self.testImgLabel = Label(self.testImgFrame,image = self.TestImage)
+                             highlightthickness=2).\
+                                 grid(row=0,column=0)
+
+        self.testImgLabel = Label(self.testImgFrame,
+                                  image = self.TestImage,
+                                  text = 'Test Image Will Be Loaded Here',
+                                  relief=SUNKEN)
+        self.testImgLabel.grid(row=0,column=0)
+
         self.loadtestImg = Button(self.testImgFrame,
                                   text='Load Test Image', 
-                                  command=self.loadTestImage)
-        self.loadtestImg.grid(row=0, column=0)
+                                  command=self.loadTestImage).\
+                                       grid(row=1, column=0,sticky=W)
 
+
+        #Setting Up Segmented Image Panel
 
         self.userCommandsFrame = Frame(master,highlightbackground="green",
                                   highlightcolor="green",
-                                  highlightthickness=2)
-        self.userCommandsFrame.pack(side=BOTTOM)
+                                  highlightthickness=2).\
+                                      grid(row=1,column=2)
+
+        self.segmentedImgLabel = Label(self.userCommandsFrame,
+                                       image = self.SegmentedImage,
+                                       text = 'Segmented Image Will Be Displayed Here',
+                                       relief=SUNKEN)
+        self.segmentedImgLabel.grid(row=0,column=2)
+
         self.restoreWeightsButton = Button(self.userCommandsFrame,
                                            text='Restore Model Weights',
-                                           command=self.restoreModelWeights)
-        self.restoreWeightsButton.grid(row=0, column=0)
+                                           command=self.restoreModelWeights).\
+                                               grid(row=1, column=2,sticky=W)
 
-        self.quitButton = Button(self.userCommandsFrame,text='Quit',command=master.destroy)
-        self.quitButton.grid(row=0, column=1)
+        self.segmentTestImage = Button(self.userCommandsFrame,
+                                           text='Segment Test Image',
+                                           command=self.segmentTestImage).\
+                                               grid(row=2, column=2,sticky=W)
+
+
+        #Quit Button
+        self.quitButton = Button(self.userCommandsFrame,
+                                 text='Quit',
+                                 command=master.destroy).\
+                                     grid(row=3, column=2, sticky=W)
 
 
     def restoreModelWeights(self):
@@ -40,11 +66,20 @@ class deepLabV3_GUI:
     
     def loadTestImage(self):
         path=filedialog.askopenfilename(filetypes=[("Image Format",'.jpg'),("Image Format",'.png')])
-        im = Image.open(path)
-        tkimage = ImageTk.PhotoImage(im)
+        self.TestImage = Image.open(path)
+        tkimage = ImageTk.PhotoImage(self.TestImage)
         self.testImgLabel.configure(image=tkimage)
         self.testImgLabel.image=tkimage
-        self.testImgLabel.grid(row=1,column=0)
+        self.testImgLabel.grid(row=0,column=0)
+        #TestImage.show()
+
+    def segmentTestImage(self):
+        # self.SegmentedImage = ImplementThisFunctionForSegmentation(self.TestImage)
+        tkimage = ImageTk.PhotoImage(self.TestImage)
+        self.segmentedImgLabel.configure(image=tkimage)
+        self.segmentedImgLabel.image=tkimage
+        self.segmentedImgLabel.grid(row=0,column=2)
+
 mainWindow = Tk()
 deepLabV3 = deepLabV3_GUI(mainWindow)
 mainWindow.mainloop()
