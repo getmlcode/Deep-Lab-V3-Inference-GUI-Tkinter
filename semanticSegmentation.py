58bf017106e81186beed589b3cd08c51e4eaf5c0
siddharthsvnit <siddharthsvnit@gmail.com>
2018-12-30 02:42:10 +0530

    added code for image segmentation using Deep-Lab-V3

--- semanticSegmentation.py
+++ semanticSegmentation.py
@@ -1,5 +1,6 @@
+from preprocessing.read_data import tf_record_parser, scale_image_with_crop_padding
+from preprocessing import training
 import tensorflow as tf
-print("TF version:", tf.__version__)
 import numpy as np
 from matplotlib import pyplot as plt
 import network
@@ -7,8 +8,6 @@ slim = tf.contrib.slim
 import os
 import argparse
 import json
-from preprocessing.read_data import tf_record_parser, scale_image_with_crop_padding
-from preprocessing import training
 from PIL import Image
 
 class Dotdict(dict):
@@ -18,33 +17,41 @@ class Dotdict(dict):
      __delattr__ = dict.__delitem__
 
 class deepLab_InferenceEngine:
-    def __init__(self,modelDirectory,sess):
-        self.modelDirectory=modelDirectory
+    def __init__(self,modelDirectory):
+        self.modelDirectory=modelDirectory #Directory where necessary model checkpoints are located 
         with open(modelDirectory + '\data.json', 'r') as fp:
             self.modelConfig = json.load(fp)
             self.modelConfig = Dotdict(self.modelConfig )
-            self.class_labels = [v for v in range((self.modelConfig.number_of_classes+1))]
-            self.class_labels[-1] = 255
     
-    def restoreDeepLab(self):
-        self.testImage = np.array(Image.open(self.modelDirectory+'\\test.jpg'))
-        self.testImage = np.expand_dims(self.testImage, axis=0)
+    def segmentImage(self,inputImage):
+        self.testImage = inputImage
+        self.testImage = self.testImage.resize((self.modelConfig.crop_size,
+                                                self.modelConfig.crop_size),
+                                                Image.ANTIALIAS)
+        self.testImage = np.array(self.testImage)
+        self.testImage = np.expand_dims(self.testImage, axis=0) #This is necessary else Resnet starts bitching about dimensions
         self.testImage = tf.cast(self.testImage, tf.float32)
-        
-        
+
         self.logits_tf =  network.deeplab_v3(self.testImage,
                                              self.modelConfig,
                                              is_training=False,
                                              reuse=False)
-
+        
         self.predictions_tf = tf.argmax(self.logits_tf , axis=3)
         saver = tf.train.Saver()
         saver.restore(sess, os.path.join(self.modelDirectory, "model.ckpt"))
-        print('Model Restored')
+
+        print('Model restored, Segmentation In Progress..')
+        resultImg = sess.run(self.predictions_tf)
+        print('Segmentation Done !')
+        return resultImg[0]
+        
 
 if __name__=="__main__":
     sess = tf.Session()
-    deepLab = deepLab_InferenceEngine('D:\Acads\IISc ME\Projects\SemanticSegmentation\deepLabV3_Inference_GUI\model',
-                                      sess)
+    deepLab = deepLab_InferenceEngine(os.getcwd()+'\model')
     print(deepLab.modelConfig.crop_size)
-    deepLab.restoreDeepLab()
+    testImage = Image.open('C:\\DataSets'+'\\t140.jpg')
+    segmentedImage = deepLab.segmentImage(testImage)
+    plt.imshow(segmentedImage)
+    plt.show()
